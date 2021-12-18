aeca5969c90696396d6c8e02639a0909a14ee0b4
Hrytsai-Maksym <max.poltava777@gmail.com>
2021-12-19 00:43:33 +0200

    1

---
+++ js/mobile.js
@@ -0,0 +1,14 @@
+(() => {
+    const refs = {
+        openMenuBtn: document.querySelector(".open-mobile-menu"),
+        closeMenuBtn: document.querySelector(".mobile-menu-closed"),
+        menu: document.querySelector(".mob-menu"),
+    };
+
+    refs.open-mobile-menu.addEventListener("click", toggleMenu);
+    refs.mobile-menu-closed.addEventListener("click", toggleMenu);
+
+    function toggleMenu() {
+        refs.menu.classList.toggle("is-hidden-a");
+    }
+})();
